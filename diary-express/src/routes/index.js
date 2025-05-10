83c34f673231e7fd2cfe5a0db68a9853884095fa
HaniHe <heyuxin2288@163.com>
2025-05-11 01:39:09 +0800

    feat:PC端基础接口开发

--- diary-express/src/routes/index.js
+++ diary-express/src/routes/index.js
@@ -4,9 +4,12 @@ const router = express.Router();
 const userRoutes = require("./userRoutes");
 // const taskRoutes = require("./taskRoutes");
 const tripRoutes = require("./tripRoutes");
+const adminRoutes = require("./adminRoutes");
 
 router.use("/user", userRoutes);
-// router.use("/task", taskRoutes);
 router.use("/trip", tripRoutes);
+router.use("/admin", adminRoutes);
+
+
 
 module.exports = router;
