7c2299bff52353f2c88183732beb8beb5dead81f
Lydia <mengdigong11@gmail.com>
2025-05-11 01:23:53 +0800

    init:后端框架搭建与基础接口开发

---
+++ diary-express/src/models/trip.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const tripSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  createTime: { type: Date, default: Date.now },
+  updateTime: { type: Date, default: Date.now },
+  // userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  // userId关联到用户模型的_id字段
+  userId: { type: String, required: true },
+  username: { type: String, required: true },
+  avatar: { type: String },
+  // likeCount: { type: Number, default: 0 },
+  images: { type: [String] },
+  // 审核状态：wait-待审核，pass-审核通过，reject-审核拒绝
+  auditStatus: { type: String, default: "wait" },
+  // 审核时间
+  auditTime: { type: Date },
+  // 审核人
+  auditor: { type: String },
+  // 删除原因
+  deleteReason: { type: String },
+  // 逻辑删除标识
+  isDeleted: { type: Boolean, default: false },
+});
+
+const Trip = mongoose.model("Trip", tripSchema);
+
+module.exports = Trip;
