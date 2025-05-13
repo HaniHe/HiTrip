8aecd95c245f08e1b45af1995853e3b9ccb03538
Lydia <mengdigong11@gmail.com>
2025-05-13 21:32:43 +0800

    upd：适配安卓使用react-nativate-paper

--- diary-app/App.js
+++ diary-app/App.js
@@ -1,20 +1,38 @@
-import * as React from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider as ReduxProvider } from "react-redux"; // 为了避免命名冲突，将 Redux 的 Provider 重命名为 ReduxProvider
+import { Provider as PaperProvider } from "react-native-paper"; // 引入 React Native Paper 的 Provider
+import store from "@/store";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Provider } from "react-redux";
-import store from "store";
-import Nav from "@components/Nav/Nav";
+import Nav from "@/components/Nav";
+import Login from "@/views/login";
+import { navigationRef } from "@/utils/NavigationService";
+import Toast from "react-native-toast-message";
 
+const Stack = createNativeStackNavigator();
 
-
-function App() {
-    return (
-      <Provider store={store}>
-        <NavigationContainer>
-          <Nav></Nav>
+const App = () => {
+  return (
+    <ReduxProvider store={store}>
+      <PaperProvider>
+        <NavigationContainer ref={navigationRef}>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Main"
+              component={Nav}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
         </NavigationContainer>
-      </Provider>
-    );
-}
+        <Toast />
+      </PaperProvider>
+    </ReduxProvider>
+  );
+};
 
 export default App;
